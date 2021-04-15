4e9b8f5ffcf0515ca4904841646e08fdc3c288ca
kalaivani1984 <77589371+kalaivani1984@users.noreply.github.com>
2021-04-14 20:24:40 -0500

    initial version

---
+++ firsttestngproj/src/test/java/test/com/CalculatorTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import itexpert.Calculator;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author kalai
+ */
+public class CalculatorTest {
+    Calculator cal;
+    private static WebDriver driver;
+     private String baseUrl="https://www.indeed.com"; 
+    
+    public CalculatorTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+        cal = new Calculator();
+        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    @Test
+    public void testAdd() {
+        assertEquals(cal.add(2,2), 4);
+    }
+     @Test 
+    public void testIndeed() throws Exception{
+         driver.get("http://demo.guru99.com/test/newtours/index.php");
+         Thread.sleep(1000);
+         driver.close();
+    }
+}
